6022f7350f783e555b6eef9021cd7b3114351923
bigbadaI <72716007+bigbadaI@users.noreply.github.com>
2021-02-15 16:09:40 -0700

    Add classnames for button and create DayListItem and scss

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 import React from "react";
-
 import "components/Button.scss";
+const classname = require ("classnames");
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm"
+   const buttonClass = classname("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
    }
-
-   if (props.danger) {
-      buttonClass += " button--danger"
-   }
+   )
 
    return (
    <button 
---
+++ src/components/DayListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "components/DayListItem.scss";
+const classname = require ("classnames");
+
+export default function DayListItem(props) {
+  const dayClass = classname("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+ }
+ )
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular" >{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  )
+};
