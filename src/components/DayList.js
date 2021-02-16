7f46fa3289ca51ec55f2a412d960185b8cc3786a
bigbadaI <72716007+bigbadaI@users.noreply.github.com>
2021-02-15 17:03:52 -0700

    Update master to include DayList that uses DayListItem to iterate over multiple days

---
+++ src/components/DayList.js
@@ -0,0 +1,21 @@
+import React from "react";
+// const classname = require ("classnames");
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  let eachDay = props.days.map(day => {
+
+    console.log(day)
+    return <DayListItem 
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}  />
+  
+  })
+  return (
+    <ul>
+      {eachDay}
+    </ul>
+  )
+};
